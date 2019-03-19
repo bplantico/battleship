15cdf5404d1d25b4db2c8b6b4173a7be46993d5d
Brian Plantico <bplantico@gmail.com>
2019-03-19 15:41:01 -0600

    Adds coordinate to initialize

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -11,6 +11,12 @@ class CellTest < MiniTest::Test
     assert_instance_of Cell, cell
   end
 
+  def test_it_has_coordinate
+    cell = Cell.new("B4")
+
+    assert_equal "B4", cell.coordinate
+  end
+
 end
 
 # pry(main)> cell.coordinate
